c40c684da449e8b89eece10deee4c55d422c1d0d
Shota Minami <minami1129south@gmail.com>
2019-04-07 14:50:14 +0900

    [change] adjust default probability

--- src/onemax/individual.py
+++ src/onemax/individual.py
@@ -12,11 +12,11 @@ class Individual(object):
     '''
 
     __size = 100
-    __individual_mutate_pb = 0.4
+    __individual_mutate_pb = 0.2
     __gene_mutate_pb = 0.05
 
     @classmethod
-    def initialize(cls, size = 100, mutate = 0.4, gene_mutate = 0.05):
+    def initialize(cls, size = 100, mutate = 0.2, gene_mutate = 0.05):
         '''
         set individual's mutation probability
         '''
