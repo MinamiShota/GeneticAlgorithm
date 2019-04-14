1544b99ce71236cf4c37b1149a3a5dc2832e4870
Shota Minami <minami1129south@gmail.com>
2019-04-15 00:05:19 +0900

    [add] scheduling classes

---
+++ src/scheduling/Scheduling.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+
+from enum import IntEnum, auto
+import random
+
+class Location(IntEnum):
+    A = auto()
+    B = auto()
+    C = auto()
+    D = auto()
+    E = auto()
+    F = auto()
+    G = auto()
+
+class Occupation(IntEnum):
+    A = auto()
+    B = auto()
+    C = auto()
+    D = auto()
+    E = auto()
+    F = auto()
+    G = auto()
+
+class Matter(object):
+
+    def __init__(self, location, occupation_map, begin_time, end_time):
+        self.location = location
+        self.occupation_map = occupation_map
+        self.begin_time = begin_time
+        self.end_time = end_time
+
+    def __str__(self):
+        return f'matter: {self.begin_time} - {self.end_time} location: {self.location}'
+
+class Person(object):
+
+    __next_id = None
+
+    def __init__(self, occupation):
+        self.__initialize_id()
+
+        self.occupation = occupation
+        self.id = self.__next_id[occupation]
+        self.__next_id[occupation] += 1
+
+    @classmethod
+    def __initialize_id(self):
+        if(self.__next_id is not None):
+            return
+
+        self.__next_id = {}
+        for occupation in Occupation:
+            self.__next_id[occupation] = 0
+
+
+class Schedule(object):
+
+    matters = None
+    persons = None
+    occupation_person_count = {}
+
+    @classmethod
+    def initialise(cls, matters, persons):
+        cls.matters = matters
+        cls.persons = persons
+        for occupation in Occupation:
+            cls.occupation_person_count[occupation] = 0
+
+        for person in cls.persons:
+            cls.occupation_person_count[person.occupation] += 1
+
+
+    def __init__(self):
+        self.__create_schedule_list()
+        self.__create_random_schedule()
+
+    def __create_schedule_list(self):
+        self.list_map = {}
+        for occupation in Occupation:
+            self.list_map[occupation] = []
+            for matter in self.matters:
+                self.list_map[occupation].extend([0 for _ in range(matter.occupation_map[occupation])])
+
+    def __create_random_schedule(self):
+        for occupation in Occupation:
+            occupation_list = self.list_map[occupation]
+            person_count = self.occupation_person_count[occupation]
+            if(person_count == 0):
+                continue
+
+            for i in range(len(occupation_list)):
+                occupation_list[i] = random.randint(0, person_count - 1)
+
+    def __eval_duplicate(self):
+        duplicate = 0
+        for occupation in Occupation:
+            begin = 0
+            for matter in self.matters:
+                end = begin + matter.occupation_map[occupation]
+                matter_list = self.list_map[occupation][begin:end]
+                dup_list = [x for x in set(matter_list) if matter_list.count(x) > 1]
+                duplicate += len(dup_list)
+                begin = end
+
+        return duplicate
+
+
+    def evaluate(self):
+        return self.__eval_duplicate()
+
+    def console_out(self):
+        iterators = {}
+        for occupation in Occupation:
+            iterators[occupation] = iter(self.list_map[occupation])
+
+        for matter in self.matters:
+            print(matter)
+
+            for occupation in Occupation:
+                print(occupation, end=": ")
+                for _ in range(matter.occupation_map[occupation]):
+                    print(next(iterators[occupation]), end=" ")
+                print()
+
+def create_matters():
+    matters = []
+    locations = [loc for loc in Location]
+    person_count = range(3)
+    times = [10, 12, 14, 16, 18, 20]
+
+    for _ in range(20):
+        location = random.choice(locations)
+        occupation_map = {}
+        for occupation in Occupation:
+            occupation_map[occupation] = random.choice(person_count)
+        time = random.sample(times, 2)
+        begin_time = min(time)
+        end_time = max(time)
+        matters.append(Matter(location, occupation_map, begin_time, end_time))
+
+    return matters
+
+def create_persons():
+    persons = []
+
+    for occupation in Occupation:
+        persons.extend([Person(occupation) for _ in range(20)])
+
+    return persons
+
+def main():
+    Schedule.initialise(create_matters(), create_persons())
+    schedules = [Schedule() for _ in range(100)]
+    for schedule in schedules:
+        schedule.console_out()
+        print(schedule.evaluate())
+
+if __name__ == "__main__":
+    main()
